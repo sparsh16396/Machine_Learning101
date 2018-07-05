e2ef683fb99b88a057cd466a44601f32f65608a5
sparsh16396 <40165123+sparsh16396@users.noreply.github.com>
2018-07-05 11:13:08 +0530

    Update linearregresion.py

--- sklearn/linearregresion.py
+++ sklearn/linearregresion.py
@@ -2,19 +2,21 @@
 
 
 import matplotlib.pyplot as plt
-import pandas as pd
+#import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
-xdata= np.asarray([1,2,2,6,3,6,9,5,6,11,8,51,12,55,18,52,45,12,65,12])
-ydata= np.asarray([8,9,12,56,24,21,32,12,7,56,12,65,35,65,41,1,21,4,44,88])
+xdata= np.asarray([1.2,2.6,3.6,9.5,6.11,8.51,12.55,18.52,45.12,65.12])
+ydata= np.asarray([8.9,12.56,24.21,32.12,7.56,12.65,35.65,41.1,21.4,44.88])
 xdata=xdata.reshape(10,1)
 ydata=ydata.reshape(10,1)
+print(xdata)
 a= np.linspace(0,50,20)
-algo=LinearRegression()
+a=a.reshape(20,1)
+algo= LinearRegression()
 algo.fit(xdata,ydata)
-plt.scatter(xdata,ydata,lable='training data')
+plt.scatter(xdata,ydata,lable="training data")
 b= algo.predict(a)
-plt.plot(a,b,label='test response')
+plt.plot(a,b,label="test response")
 z=np.asarray([20,2])
 z=z.reshape(1,1)
 print(algo.predict(z))
